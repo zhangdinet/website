31c212a958754612b4f0c25fdab5c923b6f6a9c7
zhangdinet <zhangdinet@gmail.com>
2016-12-05 14:37:45 +0800

    feat: add zookeeper

---
+++ test/src/test/java/com/zhangdi/Main.java
@@ -0,0 +1,10 @@
+package com.zhangdi;
+
+/**
+ * Created by hzzhandi3 on 2016/12/5.
+ */
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
---
+++ test/src/test/java/com/zhangdi/ZooKeeperTest.java
@@ -0,0 +1,37 @@
+package com.zhangdi;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.testng.annotations.Test;
+
+/**
+ * Created by hzzhandi3 on 2016/12/5.
+ */
+public class ZooKeeperTest {
+
+    private static String connectionString = "127.0.0.1:2181,127.0.0.1:3181,127.0.0.1:4181";
+
+    private static ExponentialBackoffRetry retryPolicy = new ExponentialBackoffRetry(1000, 3);
+
+
+    @Test
+    public void testZK() throws Exception {
+        CuratorFramework clientSimple = CuratorFrameworkFactory.newClient(connectionString, retryPolicy);
+
+        CuratorFramework client = CuratorFrameworkFactory.builder()
+                .connectString(connectionString)
+                .retryPolicy(retryPolicy)
+                .connectionTimeoutMs(20000)
+                .sessionTimeoutMs(20000)
+                .build();
+
+        client.start();
+
+        //client.create().forPath("/test","demo".getBytes());
+        String s = new String(client.getData().forPath("/test"));
+        int a = 10;
+    }
+
+
+}
