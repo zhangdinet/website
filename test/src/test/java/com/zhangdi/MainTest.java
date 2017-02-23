6f5fdc7aa50e37b36055ab375892a6d4c4f4ce89
zhangdinet <zhangdinet@gmail.com>
2017-02-23 19:07:10 +0800

    feat: add random demo

--- test/src/test/java/com/zhangdi/MainTest.java
+++ test/src/test/java/com/zhangdi/MainTest.java
@@ -3,6 +3,9 @@ package com.zhangdi;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by hzzhandi3 on 2016/12/15.
  */
@@ -11,6 +14,12 @@ public class MainTest {
     private static Logger logger = LoggerFactory.getLogger(MainTest.class);
 
     public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList();
+        for(Integer l : list){
+            System.out.print(l);
+        }
+        System.out.print("end");
 
     }
 
